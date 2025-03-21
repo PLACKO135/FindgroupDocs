13c7ce8306ff53eddb082a399856868bfdacc83d
Placko135 <papplaszlolevente@gmail.com>
2025-03-21 07:55:35 +0100

    first commit

---
+++ app/_meta.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  introduction: "Bemutatkozás 👋",
+  sysspec: "Rendszer követelmények 📚",
+  users: "Felhasználóknak",
+  devs: "Fejlesztőknek",
+};
